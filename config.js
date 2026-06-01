4116f07513fa84ecfba63caad48e2ecbd1d610bd
eradzeluka80-eng <eradzeluka80@gmail.com>
2026-06-01 20:11:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,14 +8,14 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['393312171655', '𝐄𝐒𝐒𝐄𝐍𝐓𝐈𝐀𝐋 𝐁Ꮻ𝐓', true],
-  ['377607935810'],
-  ['393756447945'],
-  ['393312171655']
-] 
+  ['3296026808', '𝐄𝐒𝐒𝐄𝐍𝐓𝐈𝐀𝐋 𝐁Ꮻ𝐓', true],
+  ['212657895110'],
+  
 
-global.suittag = ['393312171655'] 
-global.prems = ['393312171655'] 
+
+
+global.suittag = ['3296026808'] 
+global.prems = [3296026808'] 
 
 global.packname = '𝐄𝐒𝐒𝐄𝐍𝐓𝐈𝐀𝐋'
 global.author = '𝐁Ꮻ𝐓'
